ddd61fdc5f5078dedd46c915b12ca5927cd98285
showeimer <showeimer@gmail.com>
2017-08-30 00:33:58 -0400

    fetchJsonp install, and experimented with package against WalMart API

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 
+// NPM Package that fetches JSONP, Wal-Mart has NO-CORS issue
+import fetchJsonp from 'fetch-jsonp';
+
 class BaseLayout extends Component {
+  constructor(props) {
+    super(props);
+
+  };
+
+  componentDidMount() {
+    fetchJsonp('http://api.walmartlabs.com/v1/search?apiKey=5q8hhdk3vxe8xu92m8c7ntrs&query=strategy%20board%20games')
+    .then((response) => {return response.json()})
+    .then((data) => {console.log(data.items)})
+    .catch((error) => {console.log(error)})
+  };
+
   render() {
+
     return (
       <div>
         <nav>
